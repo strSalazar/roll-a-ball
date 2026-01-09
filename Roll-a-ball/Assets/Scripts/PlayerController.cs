079452fc6b57f881cb4ffeb907bf55c228a23862
strSalazar <alejandrosalazargonzalez2004@gmail.com>
2026-01-09 18:50:54 +0000

    empezando con los scripts

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+}
